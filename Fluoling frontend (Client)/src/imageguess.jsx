924cd397319fe7237647d74d89e5ff62e4ba1e35
Felix-Annaman <annaman819.fa@gmail.com>
2024-03-01 14:49:29 +0000

    testing out multi-player feature, have added to previous code and added comments to help me to understand what each logic is.

--- Fluoling frontend (Client)/src/imageguess.jsx
+++ Fluoling frontend (Client)/src/imageguess.jsx
@@ -1,117 +1,91 @@
 import React, { useState, useEffect } from 'react';
 import './imageguess.css'
-//import enWordArr from '../words(en)';
 
 const APIkey = 'caailYVBDQ7hpb4Ls9S49MSR0NrCdykg';
 
-//const wordArr = ['bear', 'fox', 'turtle', 'flamingo'];
-
 function ImageGuess() {
+    // State variables
+    const [currentWord, setCurrentWord] = useState(''); // The current word to guess
+    const [imageURL, setImageURL] = useState(''); // The URL of the image to display
+    const [playerControl, setPlayerControl] = useState('hidden'); // Controls the visibility of the player input
+    const [startButton, setStartButton] = useState('Start'); // The text of the start button
+    const [gameFeedback, setGameFeedback] = useState(''); // Feedback message to the player
+    const [words, setWords] = useState([]); // The list of words to guess
 
-    const [currentWord, setCurrentWord] = useState('');
-    const [imageURL, setImageURL] = useState('');
-    const [playerControl, setPlayerControl] = useState('hidden');
-    const [startButton, setStartButton] = useState('Start');
-    const [gameFeedback, setGameFeedback] = useState('');
-    const [words, setWords] = useState([]);
-
+    // Fetch the list of words from the server when the component mounts
     useEffect(() => {
         fetch('http://localhost:4000/api/words/english')
-          .then(response => response.json())
-          .then(data => {
-            setWords(data);
-          })
-          .catch(error => {
-            console.error('Error fetching words:', error);
-          });
-      }, []);
+            .then(response => response.json())
+            .then(data => {
+                setWords(data);
+            })
+            .catch(error => {
+                console.error('Error fetching words:', error);
+            });
+    }, []);
 
+    // Start a new game
     const startGame = () => {
-
-        console.log(words);
-
-
+        // Choose a random word from the list
         const randomIndex = Math.floor(Math.random() * words.length);
         const randomWord = words[randomIndex];
 
-        console.log(randomWord);
-
+        // Update the state with the new word
         setCurrentWord(randomWord);
         setPlayerControl('visible');
         setStartButton('Restart');
-        
-    
 
+        // Fetch an image for the word from the Giphy API
         const queryURL = `https://api.giphy.com/v1/gifs/search?api_key=${APIkey}&q=${randomWord}&limit=1&offset=0&rating=g&lang=en&bundle=messaging_non_clips`;
 
         fetch(queryURL)
-
-        .then((response) => 
-            // return 
-            response.json())
-        .then((data) => {
-        // console.log(data.data[0]['images']['original']['url']);
-        setImageURL(data.data[0]['images']['original']['url']);
-        });
-
+            .then(response => response.json())
+            .then(data => {
+                // Update the state with the new image URL
+                setImageURL(data.data[0]['images']['original']['url']);
+            });
     };
 
+    // Check the player's guess
     const checkWord = () => {
-
         const userAnswer = document.getElementById('playerInput').value;
 
-        // console.log(userAnswer);
-
         if (userAnswer === currentWord) {
+            // If the guess is correct, show a success message and start a new game after 2 seconds
             setGameFeedback(currentWord + ' is correct!');
             setTimeout(() => {
-                // nextQuestion();
                 startGame();
                 setGameFeedback('');
-              }, "2000");
-        }   else {
-                setGameFeedback('Try again!')
-            };
+            }, "2000");
+        } else {
+            // If the guess is incorrect, show an error message
+            setGameFeedback('Try again!')
+        };
     };
 
-
+    // Render the game UI
     return (
-
         <div className="gameMain">
-
             <h1 className="mb-5">Let the games begin...</h1>
-
             <div id="gameBox">
-
                 <div id="imageBox">
                     {imageURL && <img src={imageURL} alt="Giphy" />}
                 </div>
-
-                {/* <div id="wordBox">{currentWord}</div> */}
-
                 <div id="guessBox" className="mt-5">
-
                     <div className="gameFeedback my-5">
-
                         {gameFeedback}
-
                     </div>
-
                     <div id="playerControl" className={playerControl}>
-
                         <input id="playerInput" type="type" />
                         <button onClick={checkWord} id="answerSubmit">Enter</button>
-
                     </div>
-
                 </div>
-                
             </div>
-
-            <button onClick={startGame} className="mt-5" >{startButton}</button>
-
+            <button onClick={startGame} className="mt-5">{startButton}</button>
         </div>
     );
 }
 
 export default ImageGuess;
+
+
