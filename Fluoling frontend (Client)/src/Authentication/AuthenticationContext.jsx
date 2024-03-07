6dd2678db37a99f99ec19327760b484935d0a10e
Filip Kotanski <filip.kotanski@gmail.com>
2024-03-07 15:45:43 +0000

    Saving latest changes

--- Fluoling frontend (Client)/src/Authentication/AuthenticationContext.jsx
+++ Fluoling frontend (Client)/src/Authentication/AuthenticationContext.jsx
@@ -46,6 +46,7 @@ export const AuthenticationProvider = ({ children }) => {
         if (responseData.success) {
           setEmail(responseData.email); // Set the authenticated user
           //setIsAuthenticated(true); // Set authentication status to true
+          
           sessionStorage.setItem("email",responseData.email);
           sessionStorage.setItem("isAuthenticated", "true");
           setErrorMessage(''); // Clear any previous error message
@@ -97,6 +98,7 @@ export const AuthenticationProvider = ({ children }) => {
 
           const responseData = await response.json();
 
+          console.log(responseData.user);
           
           sessionStorage.setItem("user",JSON.stringify(responseData.user));
           return responseData.user;
